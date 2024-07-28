e8a51a32c2a7b71080615923aef42ca394a0e4ef
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-07-28 20:07:04 +0300

    add projects file

---
+++ src/js/projects.js
@@ -0,0 +1,16 @@
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/swiper-bundle.min.css';
+
+
+const  swiper = new Swiper(".projects-swiper-container", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+});
+    
