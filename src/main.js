530326b6d726757b5bc19f731f6699f1536c9f1d
Керівник відділу Луцьк <lush@kpp.corp.com>
2024-07-29 13:26:04 +0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ import {
 import * as faq from './js/faq.js';
 import * as reviews from './js/reviews.js';
 import * as projects from './js/projects.js';
+import * as footer from './js/footer.js';
 
 // import {
 //   swiper
