e2f207b2a431c50090affa6099211481933c5e1d
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-07-29 14:40:55 +0300

    changed project js

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,16 +1,38 @@
 // import Swiper JS
 import Swiper from 'swiper';
 // import Swiper styles
-import 'swiper/swiper-bundle.min.css';
+import 'swiper/css';
+import 'swiper/css/autoplay';
 
-
-const  swiper = new Swiper(".projects-swiper-container", {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    freeMode: true,
-    navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
+document.addEventListener('DOMContentLoaded', function() {
+    // Swiper: Slider
+    const swiper = new Swiper('.projects-swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        },
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true
+        },
+        slidesPerView: 1,
+        spaceBetween: 20,
+        breakpoints: {
+            1440: {
+                slidesPerView: 1,
+                spaceBetween: 30
             },
+            768: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            375: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            }
+        }
+    });
 });
-    
+
