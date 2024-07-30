7a56a55a731da460237398910e0532cfaab90425
AndriyGoncharuk <160170957+AndriyGoncharuk@users.noreply.github.com>
2024-07-30 10:08:55 +0300

    acc

--- src/main.js
+++ src/main.js
@@ -13,6 +13,7 @@ import * as faq from './js/faq.js';
 import * as reviews from './js/reviews.js';
 import * as projects from './js/projects.js';
 import * as footer from './js/footer.js';
+import * as aboutme from './js/about-me.js';
 
 // import {
 //   swiper
