1efb74441e0039de88c897c08e972254978f5a47
Bohdan <bosrok.ke@gmail.com>
2024-07-30 12:28:27 +0100

    Accordion

--- src/js/fag.js
+++ src/js/fag.js
@@ -1,21 +1,20 @@
+import Accordion from 'accordion-js';
+import 'accordion-js/dist/accordion.min.css';
+
 document.addEventListener('DOMContentLoaded', function () {
-  const triggers = document.querySelectorAll('.ac-trigger');
-
-  triggers.forEach(trigger => {
-    trigger.addEventListener('click', function () {
-      const parent = this.closest('.faq-item');
-      const panel = parent.querySelector('.ac-panel');
-      const icon = parent.querySelector('.icon-answer use');
-
-      if (parent.classList.contains('is-active')) {
-        parent.classList.remove('is-active');
-        panel.style.height = '0';
-        icon.setAttribute('href', '../img/icons.svg#icon-arrow-down');
-      } else {
-        parent.classList.add('is-active');
-        panel.style.height = panel.scrollHeight + 'px';
-        icon.setAttribute('href', '../img/icons.svg#icon-arrow-up');
-      }
-    });
+  new Accordion('.accordion-container', {
+    duration: 300,
+    elementClass: 'faq-item',
+    triggerClass: 'ac-trigger',
+    panelClass: 'ac-panel',
+    activeClass: 'is-active',
+    beforeOpen: item => {
+      const icon = item.querySelector('.icon-answer use');
+      icon.setAttribute('href', '/img/icons.svg#icon-arrow-up');
+    },
+    beforeClose: item => {
+      const icon = item.querySelector('.icon-answer use');
+      icon.setAttribute('href', '/img/icons.svg#icon-arrow-down');
+    },
   });
 });
