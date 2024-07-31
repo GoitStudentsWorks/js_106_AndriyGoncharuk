ee27ff66f49ca014d202803ec5f55794eb0a6564
uspenofilm <mariia.terebus@gmail.com>
2024-07-31 19:06:44 +0300

    update accordion add swiper

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -1,12 +1,34 @@
 import Accordion from 'accordion-js';
 import 'accordion-js/dist/accordion.min.css';
 
+import Swiper from 'swiper';
+import { Navigation, Keyboard } from 'swiper/modules';
+import 'swiper/css';
+
 new Accordion('.about-me-accordion-container', {
-    duration: 400,
-    showMultiple: true,
-  });
+  duration: 400,
+  showMultiple: true,
+  openOnInit: [0],
+});
 
-// import Swiper from 'swiper';
-// import 'swiper/css';
-
-// const swiper = new Swiper('.about-me-swiper');
+const aboutMeSwiper = new Swiper('.about-me-swiper', {
+  modules: [Navigation, Keyboard],
+  loop: true,
+  slidesPerView: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3,
+    },
+    1440: {
+      slidesPerView: 6,
+    },
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+    pageUpDown: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+});
